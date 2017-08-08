7ac347d317e262e9d5b2f6607e3bc01ca4b0db49
Nelio Alves <acenelio@gmail.com>
2017-08-08 07:46:50 -0300

    skybox, luz, fogueira, personagem

---
+++ Assets/Scripts/SkeletonMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonMove : MonoBehaviour {
+
+    public float velocidade = 1;
+    public List<Vector3> lista = new List<Vector3>();
+
+    int destino;
+
+	// Use this for initialization
+	void Start () {
+        destino = 0;
+        StartCoroutine("MoverAteProximoPonto");
+    }
+
+    IEnumerator MoverAteProximoPonto() {
+        transform.LookAt(lista[destino]);
+
+        while (transform.position != lista[destino]) { 
+            transform.position = Vector3.MoveTowards(transform.position, lista[destino], velocidade * Time.deltaTime);
+            yield return null;
+        }
+
+        destino++;
+        if (destino >= lista.Count) {
+            destino = 0;
+        }
+
+        StartCoroutine("MoverAteProximoPonto");
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
